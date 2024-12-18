3481b75a90166bfdd7fecba506ee704c6eedc3b9
bar2011 <bareinav10@gmail.com>
2024-12-18 21:18:24 +0200

    Add small CSS changes to Messages page

--- src/app/messages/_components/ConversationList.tsx
+++ src/app/messages/_components/ConversationList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { CSSProperties, useState } from "react";
 
 type ConversationView = {
   left: string;
@@ -23,7 +23,7 @@ export default function ConversationList({
           return (
             <div
               className={
-                "w-full bg-white py-2 pl-4" +
+                "w-full bg-white py-2 pl-4 text-sm font-medium" +
                 " bg-opacity-" +
                 (actionIndex === index ? transparency : "100")
               }
@@ -45,10 +45,16 @@ export default function ConversationList({
         {conversations.map((conversation, index) => {
           return (
             <div
-              className={
-                "w-full bg-white py-2" +
-                " bg-opacity-" +
-                (actionIndex === index ? transparency : "100")
+              className="w-full border-x-8 border-white bg-white py-2 text-sm text-muted-foreground"
+              style={
+                {
+                  "--tw-bg-opacity":
+                    actionIndex === index ? transparency : "100",
+                  "--tw-border-opacity":
+                    actionIndex === index
+                      ? (parseInt(transparency) / 100.0).toString()
+                      : "100",
+                } as CSSProperties
               }
               onMouseEnter={() => {
                 setActionIndex(index);
@@ -69,7 +75,7 @@ export default function ConversationList({
           return (
             <div
               className={
-                "w-full bg-white py-2 pr-6 text-right" +
+                "w-full bg-white py-2 pr-6 text-right text-sm text-muted-foreground" +
                 " bg-opacity-" +
                 (actionIndex === index ? transparency : "100")
               }
